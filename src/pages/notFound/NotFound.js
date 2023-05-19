ce9fa2e1bc6dd12ac5c67fd74c475b196f5cb54b
dylanpsx <85257753+dylanpsx@users.noreply.github.com>
2023-05-19 21:35:52 +0200

    Update NotFound.js

--- src/pages/notFound/NotFound.js
+++ src/pages/notFound/NotFound.js
@@ -7,10 +7,10 @@ export default function NotFound() {
 		<div className='notFound'> 
 			<Header />
 			<div className="notFound_infos">
-				<h1 className='notFound_infos_title'>404</h1> //affiche le titre de la page d'erreur 404
+				<h1 className='notFound_infos_title'>404</h1>
 				<p className='notFound_infos_content'>La page que vous recherchez n'existe pas.</p> 
 			</div>
-				<Link className='notFound_infos_return' to='/'>Retourner sur la page d'accueil</Link>
+				<Link className='notFound_infos_return' to='/Kasa/'>Retourner sur la page d'accueil</Link>
 	
 		</div>
 	)
